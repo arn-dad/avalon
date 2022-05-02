becfa8eac95682125a4cee77469e7347d62c9e8e
arn-dad <dadayanaa@yahoo.com>
2022-05-02 19:47:52 +0400

    git pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './scripts'
+import TopBar from './sections/topbar';
+import Header from './sections/header';
+import Hero from './sections/hero';
+import Main from './sections/main';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TopBar/>
+     <Header/>
+     <Hero />
+     <Main />
     </div>
   );
 }
